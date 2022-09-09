d15c71ff3232e7351c46f0371be4b962d3cf6889
Oluwatos94 <akinbowat@gmail.com>
2022-09-09 11:12:44 -0700

    variable and while loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -12,10 +10,9 @@ int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; lw++)
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 		putchar('\n');
 
 	return (0);
 }
-
