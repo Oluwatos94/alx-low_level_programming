c60cfead7460cee013f38592d43c60a9ee6f3470
Oluwatos94 <akinbowat@gmail.com>
2022-09-13 02:33:25 -0700

    The function and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Oluwatos" followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	_putchar('O');
+	_putchar('l');
+	_putchar('u');
+	_putchar('w');
+	_putchar('a');
+	_putchar('t');
+	_putchar('o');
+	_putchar('s');
+
+	return (0);
+}
+
