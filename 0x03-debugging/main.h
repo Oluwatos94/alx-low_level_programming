c350226e6c68672c5df02d0c5b22880a04a2a369
Oluwatos94 <akinbowat@gmail.com>
2022-09-15 14:32:05 -0700

    My first debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,16 +3,9 @@
 
 #include <stdio.h>
 
-/**
- * file: Hearder
- * Auth: Oluwatos
- */
-
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-print_remaining_days();
 
 #endif /* MAIN_H */
-
