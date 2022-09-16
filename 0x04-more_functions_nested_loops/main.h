dc89246f262d09a5d5e5019f067a944c7f1d1773
Oluwatos94 <akinbowat@gmail.com>
2022-09-16 11:55:29 -0700

    more on functions and nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
-/**
- * file: main.h
- * Auth: Oluwatos
- * Desc: Header containing functions
- */
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
