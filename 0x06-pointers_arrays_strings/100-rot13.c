61dc12d59ceaf70440d47d4bc449b2a7bbef6716
Oluwatos94 <akinbowat@gmail.com>
2022-09-22 07:42:39 -0700

    more on C-pointers

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - Encode a string.
+ * @str: The string to be encoded.
+ *
+ * Return: the pointer dest
+ */
+
+char *rot13(char *str)
+{
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZabcdefghijklm";
+
+	while (*(str + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(str + count) == alphabet[i])
+			{
+				*(str + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (str);
+}
